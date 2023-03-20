d7003f7768c6eadc9f75ab42719a1524c42c8530
Xiong Wang <hero_wong@outlook.com>
2023-03-20 23:51:17 +0800

    fix unit test

--- tests/HeySiri.Core.Tests/FakeDateTimeProvider.cs
+++ tests/HeySiri.Core.Tests/FakeDateTimeProvider.cs
@@ -4,7 +4,7 @@ public class FakeDateTimeProvider : IDateTimeProvider
 {
     public FakeDateTimeProvider()
     {
-        UtcNow = new DateTime(2023, 3, 8, 23, 10, 45);
+        UtcNow = new DateTime(2023, 3, 8, 23, 10, 45, DateTimeKind.Utc);
     }
 
     public DateTime UtcNow { get; }
